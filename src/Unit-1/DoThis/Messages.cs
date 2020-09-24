c9aaadbd88043b6c2d2cbfd7e9ae871821c9854c
Carlos Luna <carlos.gil.luna@gmail.com>
2020-09-24 08:57:39 +0100

    Carlos Luna changes

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+
+    public class Messages
+    {
+
+        #region Neutral/system messages
+
+        /// <summary>
+        /// Marker class to continue processing.
+        /// </summary>
+        public class ContiunueProcessing { }
+
+        #endregion
+
+        #region Success messages
+
+        public class InputSuccess
+        {
+            public string Reason { get; private set; }
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        #endregion
+
+        #region Error messages
+
+        public class InputError
+        {
+
+            public string Reason { get; private set; }
+
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+    }
+
+    #endregion
+}
--- src/Unit-2/DoThis/Main.Designer.cs
+++ src/Unit-2/DoThis/Main.Designer.cs
@@ -28,36 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cpuButton = new System.Windows.Forms.Button();
+            this.memoryButton = new System.Windows.Forms.Button();
+            this.diskButton = new System.Windows.Forms.Button();
+            this.pauseResumeButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
             this.SuspendLayout();
             // 
             // sysChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.sysChart.ChartAreas.Add(chartArea1);
+            chartArea3.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea3);
             this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.sysChart.Legends.Add(legend1);
+            legend3.Name = "Legend1";
+            this.sysChart.Legends.Add(legend3);
             this.sysChart.Location = new System.Drawing.Point(0, 0);
+            this.sysChart.Margin = new System.Windows.Forms.Padding(4);
             this.sysChart.Name = "sysChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.sysChart.Series.Add(series1);
-            this.sysChart.Size = new System.Drawing.Size(684, 446);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.sysChart.Series.Add(series3);
+            this.sysChart.Size = new System.Drawing.Size(912, 549);
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
+            // cpuButton
+            // 
+            this.cpuButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cpuButton.Location = new System.Drawing.Point(762, 374);
+            this.cpuButton.Name = "cpuButton";
+            this.cpuButton.Size = new System.Drawing.Size(126, 44);
+            this.cpuButton.TabIndex = 1;
+            this.cpuButton.Text = "CPU";
+            this.cpuButton.UseVisualStyleBackColor = true;
+            this.cpuButton.Click += new System.EventHandler(this.cpuButton_Click);
+            // 
+            // memoryButton
+            // 
+            this.memoryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.memoryButton.Location = new System.Drawing.Point(762, 426);
+            this.memoryButton.Name = "memoryButton";
+            this.memoryButton.Size = new System.Drawing.Size(128, 41);
+            this.memoryButton.TabIndex = 2;
+            this.memoryButton.Text = "MEMORY";
+            this.memoryButton.UseVisualStyleBackColor = true;
+            this.memoryButton.Click += new System.EventHandler(this.memoryButton_Click);
+            // 
+            // diskButton
+            // 
+            this.diskButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.diskButton.Location = new System.Drawing.Point(762, 478);
+            this.diskButton.Name = "diskButton";
+            this.diskButton.Size = new System.Drawing.Size(128, 41);
+            this.diskButton.TabIndex = 3;
+            this.diskButton.Text = "DISK";
+            this.diskButton.UseVisualStyleBackColor = true;
+            this.diskButton.Click += new System.EventHandler(this.diskButton_Click);
+            // 
+            // pauseResumeButton
+            // 
+            this.pauseResumeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pauseResumeButton.Location = new System.Drawing.Point(762, 280);
+            this.pauseResumeButton.Name = "pauseResumeButton";
+            this.pauseResumeButton.Size = new System.Drawing.Size(126, 44);
+            this.pauseResumeButton.TabIndex = 4;
+            this.pauseResumeButton.Text = "PAUSE";
+            this.pauseResumeButton.UseVisualStyleBackColor = true;
+            this.pauseResumeButton.Click += new System.EventHandler(this.pauseResumeButton_Click);
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 446);
+            this.ClientSize = new System.Drawing.Size(912, 549);
+            this.Controls.Add(this.pauseResumeButton);
+            this.Controls.Add(this.diskButton);
+            this.Controls.Add(this.memoryButton);
+            this.Controls.Add(this.cpuButton);
             this.Controls.Add(this.sysChart);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Main";
             this.Text = "System Metrics";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
@@ -70,6 +124,10 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+        private System.Windows.Forms.Button cpuButton;
+        private System.Windows.Forms.Button memoryButton;
+        private System.Windows.Forms.Button diskButton;
+        private System.Windows.Forms.Button pauseResumeButton;
     }
 }
 
